527b286340563dbc9c31eb1dcced8b20000e07e4
Timophey86 <tima121212@gmail.com>
2021-02-26 17:54:16 +0200

    added geid to mail design

--- js/apps/mail/pages/email-app.cmp.js
+++ js/apps/mail/pages/email-app.cmp.js
@@ -2,15 +2,15 @@ export default {
   template: `
     <div class="mail-app-main-container">
     <div class="mail-side-actions">
-    <table class=buttons>
-    <tr>
+    <!-- <table class=buttons> -->
+    <!-- <tr>
         <td><router-link :to="'/mail-app/compose'"><button>compose</button></router-link> </tr>
       <tr> <td><router-link :to="'/mail-app/inbox'"><button>inbox</button></router-link> </tr> 
        <tr><td><router-link :to="'/mail-app/sent'"><button>sent</button></router-link> </tr> 
-</table>
-                <!-- <router-link :to="'/mail-app/compose'"><button>compose</button></router-link> 
-                <router-link :to="'/mail-app/inbox'"><button>inbox</button></router-link> 
-                <router-link :to="'/mail-app/sent'"><button>sent</button></router-link>  -->
+</table> -->
+                <router-link :to="'/mail-app/compose'"><div class="action-link">compose</div></router-link> 
+                <router-link :to="'/mail-app/inbox'"><div class="action-link">inbox</div></router-link> 
+                <router-link :to="'/mail-app/sent'"><div class="action-link">sent</div></router-link> 
             </div>
      <router-view />
      </div>
